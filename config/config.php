499291aa97a15dd4ca9bebee22b16d5fa0005218
Muhammad Taufiq <c4hpunke@yahoo.com>
2026-04-14 14:26:05 +0700

    Create config file for database connection

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+// config.php
+
+$db_host = 'localhost';
+$db_username = 'root';
+$db_password = '';// your password
+$db_name = 'webgis';
+
+$conn = new mysqli($db_host, $db_username, $db_password, $db_name);
+
+if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+}
